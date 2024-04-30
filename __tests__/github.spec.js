b2f126a4e1e5a44c940035df35d6d46bcb831d7c
lucasdiniz1708 <lucasmoraes1708@gmail.com>
2024-04-30 16:16:10 -0300

    Initial commit

---
+++ __tests__/github.spec.js
@@ -0,0 +1,26 @@
+const request = require("supertest");
+
+describe("PokeAPI Locations", () => {
+  it("/get a valid Locations", async () => {
+    const response = await request("https://pokeapi.co/api/v2").get(
+      `/location/${"canalave-city"}`
+    );
+   // console.log(response.body);
+    expect(response.body.id).toBeDefined();
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe("canalave-city");
+    expect(response.body.areas.length).toBeGreaterThan(0);
+  });
+
+  it("/get a invalid location", async () => {
+    const response = await request("https://pokeapi.co/api/v2").get(
+      `/location/${"local-invalido"}`
+    );
+
+    expect(response.status).toBe(404);
+    expect(response.body).toMatchObject({});
+    
+  });
+});
+
+// https://jestjs.io/pt-BR/docs/expect#tocontainitem
